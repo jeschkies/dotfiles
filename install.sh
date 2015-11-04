5120e9ed3a0954a995cb7c73502aadc1551a1f6f
Karsten Jeschkies <karstenj@amazon.com>
2015-11-04 02:01:10 -0800

    Remove old install script.

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Install homebrew and homebrew cask
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install caskroom/cask/brew-cask
-
-# Install vim
-ln -sf ~/.dotfiles/vim/colors ~/.vim/colors
-ln -sf ~/.dotfiles/vim/vimrc ~/.vimrc
