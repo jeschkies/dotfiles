3b1be30c6ea196e9d6b2a8d2eafb96d733f030a3
Karsten Jeschkies <jeskar@web.de>
2016-06-28 16:19:52 +0200

    Make vim install script idempotent.

--- vim/install.sh
+++ vim/install.sh
@@ -12,6 +12,7 @@ mkdir -p $HOME/.vim
 cp -r $ZSH/vim/colors $HOME/.vim/colors
 
 # Install Vundle
+rm -rf $HOME/.vim/bundle/Vundle.vim
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 
 # Install plugins
