206a6f6f49ad9ed467dae6aa4c627d4481d5a044
Karsten Jeschkies <k@jeschkies.xyz>
2022-10-19 13:51:00 +0200

    Replace vim script Neovim config with lua.

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+TARGET="$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim"
+if [ ! -d "$TARGET" ]; then
+  git clone --depth 1 https://github.com/wbthomason/packer.nvim "$TARGET"
+fi
+ 
