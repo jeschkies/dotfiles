7061085a5585f0ec9fcce534ac652550adb0678e
Karsten Jeschkies <k@jeschkies.xyz>
2022-10-20 13:54:40 +0200

    Configure Neovim color theme.

--- nvim/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-TARGET="$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim"
-if [ ! -d "$TARGET" ]; then
-  git clone --depth 1 https://github.com/wbthomason/packer.nvim "$TARGET"
-fi
- 
