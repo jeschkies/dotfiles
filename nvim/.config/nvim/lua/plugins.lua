03f4ea21b52f1c0a8e9a8f71b141af1cddb4d9b6
Karsten Jeschkies <k@jeschkies.xyz>
2022-10-20 15:01:10 +0200

    Migrate linking to stow.

---
+++ nvim/.config/nvim/lua/plugins.lua
@@ -0,0 +1,40 @@
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+    PACKER_BOOTSTRAP = fn.system {
+        "git",
+        "clone",
+        "--depth",
+        "1",
+        "https://github.com/wbthomason/packer.nvim",
+        install_path,
+    }
+    print "Installing packer close and reopen Neovim..."
+    vim.cmd [[packadd packer.nvim]]
+end
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+    return
+end
+
+return packer.startup(function(use)
+  use 'wbthomason/packer.nvim' -- Package manager
+
+  -- LSP
+  use 'williamboman/mason.nvim' -- Mason package manager
+  use 'williamboman/mason-lspconfig.nvim'
+  use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
+
+  -- Color
+  use 'NLKNguyen/papercolor-theme'
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if PACKER_BOOTSTRAP then
+      require("packer").sync()
+  end
+end)
