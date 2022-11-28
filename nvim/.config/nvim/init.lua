fa36b5d98647626d9928e6db2c6bad9d6e0ace64
Karsten Jeschkies <karsten.jeschkies@grafana.com>
2022-11-28 21:12:42 +0100

    Show diagnostic on hover.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -12,6 +12,22 @@ vim.opt.number = true
 vim.opt.numberwidth = 2
 vim.wo.showbreak= '⇇'
 
+-- Show diagnostics on hover.
+vim.api.nvim_create_autocmd("CursorHold", {
+  buffer = bufnr,
+  callback = function()
+    local opts = {
+      focusable = false,
+      close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+      border = 'rounded',
+      source = 'always',
+      prefix = ' ',
+      scope = 'cursor',
+    }
+    vim.diagnostic.open_float(nil, opts)
+  end
+})
+
 --[[
 " Plugins
 call plug#begin()
