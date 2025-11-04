227a0202138e24dfa4abe065812135d0f8543a55
Karsten Jeschkies <karsten.jeschkies@grafana.com>
2025-11-04 09:49:24 +0100

    Lazily load nvm

--- nvm/path.zsh
+++ nvm/path.zsh
@@ -1,2 +1,32 @@
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# lazy load nvm 
+lazy-nvm()
+{
+  unset -f nvm node npm npx
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+}
+
+nvm()
+{
+  lazy-nvm
+  nvm $@
+}
+
+node()
+{
+  lazy-nvm
+  node $@
+}
+
+npm()
+{
+  lazy-nvm
+  npm $@
+}
+
+npx()
+{
+  lazy-nvm
+  npx $@
+}
