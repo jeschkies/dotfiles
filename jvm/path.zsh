ec2eb06af3b904f4c59555aeb27bdde9a5f62e33
Karsten Jeschkies <karsten.jeschkies@grafana.com>
2025-11-04 09:57:51 +0100

    Lazily load sdkman

--- jvm/path.zsh
+++ jvm/path.zsh
@@ -1,2 +1,25 @@
-export SDKMAN_DIR="$HOME/.sdkman"
-[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+# lazy load sdkman
+lazy-sdkman()
+{
+  unset -f sdk java mvn
+  export SDKMAN_DIR="$HOME/.sdkman"
+  [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+}
+
+sdk()
+{
+  lazy-sdkman
+  sdk $@
+}
+
+java()
+{
+  lazy-sdkman
+  java $@
+}
+
+mvn()
+{
+  lazy-sdkman
+  java $@
+}
